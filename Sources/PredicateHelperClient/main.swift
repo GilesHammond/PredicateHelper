428922be4d3da31f4ff0c3391838523de3f6a121
Giles Hammond <github@exploreandcreate.com>
2024-09-23 11:40:06 +0100

    First attempt

---
+++ Sources/PredicateHelperClient/main.swift
@@ -0,0 +1,35 @@
+import PredicateHelper
+
+import Foundation
+
+struct Item
+{
+    let startDate: Date?
+    let endDate: Date?
+    
+    @PredicateHelper static func startEndAppliesDuring(_ range: ClosedRange<Date>)
+    -> Predicate<Item> {
+        let distantPast = Date.distantPast
+        let distantFuture = Date.distantFuture
+        
+        // Comments are not transferred to the output
+        
+        return #Predicate<Item> { item in
+            ((item.startDate ?? item.endDate ?? distantFuture) < range.upperBound)
+            &&
+            ((item.endDate ?? item.startDate ?? distantPast) >= range.lowerBound)
+        }
+    }
+    
+    @PredicateHelper static func isOver(beforeDate: Date = Date.now)
+    -> Predicate<Item> {
+        let distantPast = Date.distantPast
+        
+        return #Predicate<Item> { item in
+            ((item.startDate ?? distantPast) < beforeDate)
+            &&
+            ((item.endDate ?? distantPast) >= beforeDate)
+        }
+    }
+}
+
---
+++ Sources/PredicateHelperMacros/PredicateHelperMacro.swift
@@ -0,0 +1,65 @@
+import SwiftCompilerPlugin
+import SwiftSyntax
+import SwiftSyntaxBuilder
+import SwiftSyntaxMacros
+
+/// Implementation of the `PredicateHelper` macro. This takes a static @Model class function
+/// returning #Predicate macro content and generates a matching member function performing the same
+/// logic.
+
+private let returnStringStart = "return #Predicate<"
+
+private enum PredicateHelperMacroError: Error, CustomStringConvertible {
+    case noAttachedFunction
+    case noPredicate(_ returnString: String)
+
+    var description: String {
+        switch self {
+        case .noAttachedFunction:
+            return "#PredicateHelper acts on a valid attached static function"
+        case .noPredicate(let returnString):
+            return "#PredicateHelper expects a final string beginning '\(returnStringStart)': \(returnString)"
+        }
+    }
+}
+
+public struct PredicateHelperMacro: PeerMacro {
+    public static func expansion(
+        of node: SwiftSyntax.AttributeSyntax,
+        providingPeersOf declaration: some SwiftSyntax.DeclSyntaxProtocol,
+        in context: some SwiftSyntaxMacros.MacroExpansionContext
+    ) throws -> [SwiftSyntax.DeclSyntax] {
+        guard let functionDeclaration = declaration.as(FunctionDeclSyntax.self),
+              let functionBody = functionDeclaration.body,
+              !functionBody.statements.isEmpty else {
+            throw PredicateHelperMacroError.noAttachedFunction }
+        
+        let functionName = functionDeclaration.name.text
+        let arguments = functionDeclaration.signature.parameterClause.parameters
+        let statements = functionBody.statements.map({ $0.trimmedDescription })
+        let predicateStatement = statements.last!
+        
+        guard predicateStatement.hasPrefix(returnStringStart) else {
+            throw PredicateHelperMacroError.noPredicate(predicateStatement) }
+        
+        let predicateClosure = predicateStatement.trimmingPrefix(while: { $0 != "{" })
+
+        return [DeclSyntax.init(stringLiteral: """
+            func \(functionName)(\(arguments)) -> Bool 
+            {
+                \(statements.dropLast().joined(separator: "\n"))
+            
+                let decider: (Self) -> Bool =  \(predicateClosure) 
+            
+                return decider(self)
+            }
+            """)]
+    }
+}
+
+@main
+struct PredicateHelperPlugin: CompilerPlugin {
+    let providingMacros: [Macro.Type] = [
+        PredicateHelperMacro.self,
+    ]
+}
